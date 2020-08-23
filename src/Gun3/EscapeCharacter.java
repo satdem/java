8c98a59b3473e6877bff253bb48d9fdcce444c5f
satı demirci <69872891+satdem@users.noreply.github.com>
2020-08-23 22:03:44 +0200

    guncel

--- src/Gun3/EscapeCharacter.java
+++ src/Gun3/EscapeCharacter.java
@@ -14,6 +14,7 @@ public class EscapeCharacter {
         System.out.println("pazartesi\nsalı\nçarşamba");
         System.out.println("pazartesi \tsalı");
         System.out.println("Java");
+        System.out.println("orhan");
 
         System.out.println("\"Java\"");
         System.out.println("\\java");
